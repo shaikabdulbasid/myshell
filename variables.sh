5571b8ce53d0bce382dd54606b8089bf1a446b78
shaikabdulbasid <shaikabdulbasid7@gmail.com>
2024-04-21 14:22:37 +0530

    Verified

--- variables.sh
+++ variables.sh
@@ -3,6 +3,6 @@
 PERSON1=ABDUL
 PERSON2=BASID
 
-echo "$PERSON1 :Hello How is your day $PERSON2"
-echo "$PERSON2 :AWESOME:How is your GF "
-echo "$PERSON1 :She is always beautiful "
+echo "$PERSON1 ::Hello How is your day $PERSON2"
+echo "$PERSON2 ::AWESOME:How is your GF "
+echo "$PERSON1 ::She is always beautiful "
