14ef3e73e9573a80769838906967e71e08dc78f7
shaikabdulbasid <shaikabdulbasid7@gmail.com>
2024-04-27 18:35:08 +0530

    committed

--- 05.conditions.sh
+++ 05.conditions.sh
@@ -4,7 +4,8 @@ Number=$1
 
 If [ $Number -gt 10]
 then
-echo "Given $Number is greater than 10"
+  echo "Given $Number is greater than 10"
 else
- echo "Given $Number is less than 10"
+  echo "Given $Number is less than 10"
+
 fi
